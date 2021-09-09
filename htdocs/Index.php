97d7368bd6865feb74d070c494597224e67ba414
Adrian Humphrey <humphar@bgsu.edu>
2021-09-09 16:24:31 -0400

    In class exercise 3

--- htdocs/Index.php
+++ htdocs/Index.php
@@ -5,7 +5,42 @@
 </head>
 
 <body>
-<h1><img src="images/logo.png" alt="logo">Share Your Travels</h1>
+
+<h1><img src="images/E03 images/logo.png" alt="logo">Share Your Travels</h1>
+<ul>
+  <li><a href = "#Description">Description</a></li>
+  <li><a href = "#Related">Related Photos</a></li>
+</ul>
+
+<h2>New York - Central Park</h2>
+
+<p>
+<div id = "Description"> <strong>Description</strong></div></br>
+  </br>
+
+Photo by Randy Connolly</br>
+  </br>
+
+This Photo of Conservatory Pond in <a href= "https://www.centralparknyc.org/">Central Park</a>
+  in <a href= "https://en.wikipedia.org/wiki/New_York_City">New York City</a> was taken on October 22nd, 2016 with a Canon EOS 30D camera</br>
+  </br>
+
+<a href= "images/E03 images/large-central-park.jpg"><img src="images/E03 images/central-park.jpg" alt="Central Park"></a></br>
+  </br>
+
+<div id = "Related"><strong>Related Photos</strong></div></br>
+  </br>
+
+<a href= "images/E03 images/related-large1.jpg"><img src="images/E03 images/related-square1.jpg" alt="Related Square 1"></a>
+<a href= "images/E03 images/related-large2.jpg"><img src="images/E03 images/related-square2.jpg" alt="Related Square 2"></a>
+<a href= "images/E03 images/related-large3.jpg"><img src="images/E03 images/related-square3.jpg" alt="Related Square 3"></a>
+  </br>
+  </br>
+
+<em>Copyright © 2021 Share your Travels</em>
+
+</p>
+
 </body>
 
 </html>
