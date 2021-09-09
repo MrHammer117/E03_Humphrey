2b3df8de255da22d3a023822ad85bfc43305d4b3
Adrian Humphrey <humphar@bgsu.edu>
2021-09-09 13:40:38 -0400

    In class exercise 3

---
+++ htdocs/Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<head>
+<title>Share Your Travels --New York - Central Park</title>
+</head>
+
+<body>
+<h1><img src="images/logo.png" alt="logo">Share Your Travels</h1>
+</body>
+
+</html>
